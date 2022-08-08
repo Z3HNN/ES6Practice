7bf194491cdc14edb872d4d9cf0efeb6e9166a1b
Zehn <z3hndolphin@gmail.com>
2022-08-08 16:15:32 -0400

    Practice

--- Practice.js
+++ Practice.js
@@ -2,6 +2,12 @@ function sum (a, b) {
     return a + b
 }
 
-let sum2 = (a, b) {
+let sum2 = (a, b) =>  {
     return a + b
 }
+
+let isPositive2 = number => number >= 0
+
+let randomNumber2 = () => Math.random
+
+document.addEventListener('click', () => console.log('click'))
