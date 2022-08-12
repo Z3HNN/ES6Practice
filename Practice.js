2535bc6cab483508b064662748d8560674b03039
Zehn <z3hndolphin@gmail.com>
2022-08-11 23:06:31 -0400

    p

--- Practice.js
+++ Practice.js
@@ -36,8 +36,8 @@ person.walk();
  const walk = person.walk.bind(person);
  walk(); 
 
- const square = function(number) {
-    return number * number
- }
+
 
  const square = (number) => number * number
+
+ console.log(square(5))
