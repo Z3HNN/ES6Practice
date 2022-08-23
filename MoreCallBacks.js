245884df8fe3594e004d25ccba184dabba114f48
Zehn <z3hndolphin@gmail.com>
2022-08-22 21:39:54 -0400

    p

--- MoreCallBacks.js
+++ MoreCallBacks.js
@@ -1,2 +1,14 @@
 const names = ['james', 'jess', 'lily', 'sevy']
 names.forEach((name) => console.log(name))
+
+const myForEach (arr, cb) => {
+    for(let i = 0; i < arr.length; i++){
+        const element = arr[i];
+        cb(element)
+    }
+}
+
+myForEach(names, (name) => {
+
+    console.log(name)
+})
