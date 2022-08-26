47862c88e145ab8daa0bb00006b56c6173f25e81
Zehn <z3hndolphin@gmail.com>
2022-08-25 21:35:08 -0400

    p

--- Excercises.js
+++ Excercises.js
@@ -4,4 +4,11 @@ let myString = 'practice code'
 
 //console.log(myString.charAt(0).toUpperCase() + myString.slice(1))
 
-console.log(myString.slice(0, 13).toUpperCase() + myString.slice(13))
+//console.log(myString.slice(0, 13).toUpperCase() + myString.slice(13))
+
+function capitalize(str) {
+    return str.replace(str.slice(0, 1). str.slice(0, 1).toUpperCase())
+}
+
+var result= capitalize('hello world')
+console.log(result)
