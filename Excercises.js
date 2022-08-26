08c773786ef527d16a77b371c9cc64ec4c50627a
Zehn <z3hndolphin@gmail.com>
2022-08-25 21:25:31 -0400

    p

--- Excercises.js
+++ Excercises.js
@@ -2,4 +2,4 @@ let myString = 'practice code'
 console.log(myString.charAt(0).toUpperCase())
 console.log(myString.slice(1))
 
-console.log(myString.charAt(0).toUpperCase())
+console.log(myString.charAt(0).toUpperCase() + myString.slice(1))
