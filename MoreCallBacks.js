e1b8ab32d41c3e6498d0ca052338d4930858d8a4
Zehn <z3hndolphin@gmail.com>
2022-09-19 21:00:41 -0400

    p

--- MoreCallBacks.js
+++ MoreCallBacks.js
@@ -1,4 +1,4 @@
-const firstNames = ['antaly', 'jess', 'lily', 'devy']
+const firstNames = ['anaaly', 'jess', 'lily', 'devy']
 firstNames.forEach((name) => console.log(name))
 
 const loadPokemonData = (id, cb) => {
